67986119f46ac9ba4a060379890f92295023a7d6
gravityshouldbenaut <gravityshouldbenaut@gmail.com>
2016-07-12 15:56:01 -0700

    Initial Commit

---
+++ sprective/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sprective
+//
+//  Created by Samarth Sandeep on 7/12/16.
+//  Copyright © 2016 Samarth Sandeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
